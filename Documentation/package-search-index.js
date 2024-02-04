248a0d7567efee5b2473e734038b8d1b86705d4c
Apg576 <myp0st.f@gmail.com>
2024-02-04 17:59:16 +0500

    Добавил документацию.

---
+++ Documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.controller"},{"l":"com.example.frequency"},{"l":"com.example.service"}];updateSearchResults();
---
+++ Documentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.service","l":"Calculate"},{"p":"com.example.controller","l":"Controller"},{"p":"com.example.frequency","l":"FrequencyApplication"}];updateSearchResults();
